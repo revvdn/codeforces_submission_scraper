3d2bf86324ec4a3f9cf59e338cf39c7a7907467a
revvdn <revanadnanperdana@gmail.com>
2026-05-14 19:36:55 +0700

    adding main module

--- main.py
+++ main.py
@@ -182,3 +182,94 @@ to achieve this i will break into some modules, those are :
 after things get done
 TIME TO CODE YEAH :)
 '''
+
+import os
+import re
+import sys
+import argparse
+import logging
+import requests
+import subprocess
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+API_USER_STATUS = "https://codeforces.com/api/user.status" #base link 
+API_PROBLEMSET = "https://codeforces.com/api/problemset.problems" #base link
+
+def commit_problem(pid, pinfo, sub, base_dir, rating) :
+    #step 5
+
+def update_readme(rating, pids, api_accepted, problems_info, base_dir) :
+    #step 4
+
+def construct_readme(readme_path, all_pids, api_accepted, problems_info) :
+    #step 3
+
+def fetch_data(handle) :
+    #step 2
+
+
+def main() :
+    #step 1 / main
+    '''
+    This module will get the user handle by asking them, then try to fetch and get the submission data 
+    from the codeforces based on what i have been explain above
+    im only build to fetch the "OK" or AC if in the codeforces, because in most of the case 
+    the total submission is enourmously high because not every single porblem got AC in the first try,
+    and Who's gonna display the non AC submission right :)
+    '''
+    #get the handle 
+    parser = argparse.ArgumentParser(description="codeforces submission scraper")
+    parser.add_argument("handle", help="yout codeforces handle :")
+    args = parser.parse_args()
+
+    #find the project local location, as the reference to make the readme file
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    readme_path = os.path.join(base_dir, "README.md")
+
+    #fetch the data from the cf api
+    try :
+        api_accepted, problems_info = fetch_data(args.handle)
+    except Exception as e :
+        logging.error(f"Error fetching data : {e}")
+        sys.exit(1)
+
+    #existing_pids = set(), i write this just in case the user have any solution file before using this, but i think it's not necessary
+    new_pids = set(api_accepted.keys())
+
+    '''
+    if not new_pids :
+        logging.info("no new problems to fetch")
+        sys.exit(0)
+    '''#again just in case if user had the solution before
+
+    #sorted it based on the submission time 
+    chronological_new_pids = sorted(
+        list(new_pids), 
+        key=lambda p: api_accepted[p]['creationTimeSeconds']
+    )
+
+    # current_known_pids = set(new_pids) doesn't need this anymore
+    pids_by_rating = {}
+
+    for pid in chronological_new_pids :
+        sub = api_accepted[pid]
+        pinfo = problems_info[pid]
+        rating = pinfo['rating']
+
+        logging.info(f"process {pid} {rating : {rating}}")
+
+       # current_known_pids.add(pid)
+        pids_by_rating.setdefault(rating, set()).add(pid)
+
+        update_readme(rating, pids_by_rating[rating], api_accepted, problems_info, base_dir)
+
+        construct_readme(readme_path, new_pids, api_accepted, problems_info)
+
+        commit_problem(pid, pinfo, sub, base_dir, rating)
+
+    logging.info(f"success to fetch {len(chronological_new_pids)} problems")
+
+if __name__ == "__main__" :
+    main()
