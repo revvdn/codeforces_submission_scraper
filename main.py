00d8411a2015592dec822bc2e156caf26d51453e
revvdn <revanadnanperdana@gmail.com>
2026-05-15 18:21:35 +0700

    pre testing stage

--- main.py
+++ main.py
@@ -219,7 +219,21 @@ INITIAL_TABLE_TEMPLATE = """
 
 def commit_problem(pid, pinfo, sub, base_dir, rating) :
     #step 5
-    
+    '''
+    i have been thinking that it's possible for someone who want to push the problem into github, but
+    all of the submission have their own date right, to overcome this i would set the git timestamp based on the real submission time in codeforces
+    '''
+    submission_date = datetime.fromtimestamp(sub['creationTimeSeconds']).isoformat()
+    env = os.environ.copy()
+    env["GIT_AUTHOR_DATE"] = submission_date
+    env["GIT_COMMITTER_DATE"] = submission_date
+
+    by_rating = f"problems/codeforces/by_rating/{rating}/README.md"
+    subprocess.run(["git", "add", "README.md", by_rating], cwd = base_dir, check=True)
+
+    msg = f"add codeforces {pid} - {pinfo['name']}"
+    subprocess.run(["git", "commit", "-m", msg], cwd=base_dir, env=env, check=True)
+    logging.info(f"committed {pid} with timestamp {submission_date}")
 
 def update_readme(rating, pid, api_accepted, problems_info, base_dir) :
     #step 4
